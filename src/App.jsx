847ddfadc2d9e18f9fd1327a9075c0d7b51ab116
TraXTorist <brazhnenkoyurii@gmail.com>
2025-08-27 16:07:24 +0300

    add and remove card

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,39 @@ import { useState } from "react";
 import "./App.css";
 
 export default function App() {
+  const [cards, setCards] = useState([]);
+  const [input, setInput] = useState("");
 
+  function handleInput(event) {
+    setInput(event.target.value);
+  }
+
+  function createCard() {
+    setCards([{ id: Date.now(), title: input }, ...cards]);
+  }
+
+  function renameCard(idRename) {}
+
+  function deleteCard(idRemove) {
+    setCards((cards) => cards.filter((card) => card.id !== idRemove));
+  }
 
   return (
     <div className="wrapper">
       <Header />
-      <Input />
-      <Card />
-      <Card />
-      <Card />
+      <Input onClick={createCard} value={input} onChange={handleInput} />
+
+      <ul className="card-form">
+        {cards.map((card) => {
+          return (
+            <Card
+              key={card.id}
+              title={card.title}
+              onDelete={() => deleteCard(card.id)}
+            />
+          );
+        })}
+      </ul>
     </div>
   );
 }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
 
-export default function Input() {
-
+export default function Input({ value, onChange, onClick }) {
   return (
     <form className="input-form">
-      <input type="text" id="input" placeholder="Add your items" />
-      <button className="input-btn">+</button>
+      <input
+        value={value}
+        onChange={onChange}
+        type="text"
+        id="input"
+        placeholder="Add your items"
+      />
+      <button onClick={onClick} type="button" className="input-btn">
+        +
+      </button>
     </form>
   );
 }
